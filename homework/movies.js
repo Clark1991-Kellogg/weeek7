78823f6a59845c8d86e2b5a93e19fa1bfcb3c34b
Hillary Clark <hillarysageclark@gmail.com>
2021-02-22 18:46:16 -0800

    v1

--- homework/movies.js
+++ homework/movies.js
@@ -1,14 +1,35 @@
-window.addEventListener('DOMContentLoaded', async function(event) {
+firebase.auth().onAuthStateChanged(async function(user) {
   let db = firebase.firestore()
-  let apiKey = 'your TMDB API key'
+  let apiKey = '789d6431ec6ed8b3e29367645e873c3f'
   let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
   let json = await response.json()
   let movies = json.results
   console.log(movies)
+  console.log(user)
   
+  if (user) {
+
+      db.collection('users').doc(user.uid).set({
+          name: user.displayName,
+          email: user.email
+        })
+    
+  let name = user.name
+
+   document.querySelector('.sign-in-or-sign-out').innerHTML = 
+   `
+   <div class = "text-center text-white font-bold"> Signed in as ${user.displayName} </div>
+   <button class="text-pink-500 underline sign-out">Sign Out</button>
+  `
+  document.querySelector('.sign-out').addEventListener('click', function(event) {
+    console.log('signed out')
+    firebase.auth().signOut()
+    document.location.href = 'movies.html'
+  })
+
   for (let i=0; i<movies.length; i++) {
     let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
+    let docRef = await db.collection('watched').doc(`${movie.id}-${user.uid}`).get()
     let watchedMovie = docRef.data()
     let opacityClass = ''
     if (watchedMovie) {
@@ -18,7 +39,7 @@ window.addEventListener('DOMContentLoaded', async function(event) {
     document.querySelector('.movies').insertAdjacentHTML('beforeend', `
       <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
-        <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+        <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">Watched!</a>
       </div>
     `)
 
@@ -26,9 +47,26 @@ window.addEventListener('DOMContentLoaded', async function(event) {
       event.preventDefault()
       let movieElement = document.querySelector(`.movie-${movie.id}`)
       movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
+      await db.collection('watched').doc(`${movie.id}-${user.uid}`).set({})
     }) 
   }
+  }
+
+  else {
+      // Signed out
+      console.log('signed out')
+      document.querySelector('.movies').classList.add('hidden')
+
+      let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+      let authUIConfig = {
+          signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+          ],
+          signInSuccessUrl: 'movies.html'
+        }
+        ui.start('.sign-in-or-sign-out', authUIConfig)
+      }
 })
 
 // Goal:   Refactor the movies application from last week, so that it supports
@@ -40,6 +78,9 @@ window.addEventListener('DOMContentLoaded', async function(event) {
 //         (provided) script tags for all necessary Firebase services – i.e. Firebase
 //         Auth, Firebase Cloud Firestore, and Firebase UI for Auth; also
 //         add the CSS file for FirebaseUI for Auth.
+
+//Done in HTML file
+
 // Step 2: Change the main event listener from DOMContentLoaded to 
 //         firebase.auth().onAuthStateChanged and include conditional logic 
 //         shows a login UI when signed, and the list of movies when signed
@@ -48,6 +89,8 @@ window.addEventListener('DOMContentLoaded', async function(event) {
 //         in as <name>" along with a link to "Sign out". Ensure that a document
 //         is set in the "users" collection for each user that signs in to 
 //         your application.
+
+
 // Step 3: Setting the TMDB movie ID as the document ID on your "watched" collection
 //         will no longer work. The document ID should now be a combination of the
 //         TMDB movie ID and the user ID indicating which user has watched. 
@@ -55,3 +98,5 @@ window.addEventListener('DOMContentLoaded', async function(event) {
 //         be set when the "I've watched" button on each movie is clicked. Likewise, 
 //         when the list of movies loads and is shown on the page, only the movies 
 //         watched by the currently logged-in user should be opaque.
+
+
